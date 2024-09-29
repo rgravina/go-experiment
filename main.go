424866000b7ce435c803bf684c19b3a20c169a42
Robert Gravina <robert.gravina@gmail.com>
2024-09-29 10:42:54 +1000

    Return JSON from the backend

--- main.go
+++ main.go
@@ -6,11 +6,18 @@ import (
 	"net/http"
 )
 
+type User struct {
+	Name string `json:"name" xml:"name"`
+}
+
 func main() {
 	e := echo.New()
 	web.RegisterHandlers(e)
+	u := &User{
+		Name: "Rocky Racoon",
+	}
 	e.GET("/api", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
+		return c.JSON(http.StatusOK, u)
 	})
 	e.Logger.Fatal(e.Start(":8080"))
 }
